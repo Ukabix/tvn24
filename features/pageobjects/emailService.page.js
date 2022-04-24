06a39965efdca0f18b0791a7a494bb22f3e2d875
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-04-24 17:20:18 +0200

    add email service handler POM

--- features/pageobjects/emailService.page.js
+++ features/pageobjects/emailService.page.js
@@ -1,20 +1,27 @@
 class EmailServicePage {
-  get textEmailAdress() {
-    return $('#fe_text').getValue();
+  get formEmailAdress() {
+    return $('#fe_text');
   }
-  get anchorRefresh () {
-    return $('.fa.fa-refresh.fa-fw.fa-lg')
+  get anchorRefresh() {
+    return $('.fa.fa-refresh.fa-fw.fa-lg');
   }
   get anchorSender() {
-    return $('=TVN Online <kontakt@konto.tvn.pl>');
+    return $(
+      '=TVN Online <kontakt@konto.tvn.pl>'
+    );
   }
-  get anchorSumbitSignup () {
+  get anchorSumbitSignup() {
     return $('=Przejdź do konta');
   }
   async openEmailServicePage() {
-    await browser.newWindow('https://10minutemail.net/', { windowName: '10minemail' }); // does not work in mobile env
+    await browser.newWindow(
+      'https://10minutemail.net/',
+      { windowName: '10minemail' }
+    ); // does not work in mobile env
   }
-  
+  async getEmailAddress() {
+    return await this.formEmailAdress.getValue();
+  }
 }
 
 module.exports = new EmailServicePage();
